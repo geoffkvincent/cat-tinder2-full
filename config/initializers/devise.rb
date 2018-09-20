9c42b46b4e00d7efe3eb6fb82d47315ad8e9dae2
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-20 15:11:11 -0600

    first commit

---
+++ config/initializers/devise.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Devise.setup do |config|
+  config.secret_key = 'af3dd6218371c5861fbe7f6843d169c62006a969c88d6bcbc545ece4d8752aa5e694ef5ef16121129621f4102c3b86d934a42d4a569686ffe9d29e036bcd1d84'
+end
